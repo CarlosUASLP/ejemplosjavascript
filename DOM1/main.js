c54820493649ee7ce99e3f529dea0ae59ea76b3e
Henry <henrykonnor@gmail.com>
2017-02-28 13:04:30 -0600

    Primer ejemplo del DOM1

---
+++ DOM1/main.js
@@ -0,0 +1,6 @@
+//document.write('Hola mundo JS')
+//var cajas = document.getElementsByTagName('div');
+var cajas = document.getElementsByClassName('caja')
+
+cajas[0].textContent = 'Hola mundo'
+cajas[1].innerHTML = '<b> Hola JS </b>'
